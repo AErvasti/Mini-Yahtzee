226db38979864e4774efcf17fd91d68bebd8f805
Aleksi Ervasti <n0eral00@students.oamk.fi>
2022-03-01 13:45:42 +0200

    Mini-Yahtzee repoon

---
+++ style/style.js
@@ -0,0 +1,70 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  header: {
+    marginTop: 30,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row'
+  },
+  footer: {
+    marginTop: 20,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  author: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  gameboard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gameinfo: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 18,
+    marginBottom: 5
+  },
+  item: {
+    marginHorizontal: 5,
+  },
+  flex: {
+    flexDirection: "row",
+    margin: 5
+  },
+  button: {
+    margin: 30,
+    padding: 10,
+    backgroundColor: "#73CED6",
+    width: 150,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color:"#2B2B52",
+    fontSize: 20
+  },
+  total: {
+    fontSize: 23,
+    marginBottom: 5
+  }
+});
