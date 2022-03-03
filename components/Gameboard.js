d64026b9c5340530d050254757e924f9c7860a9d
Aleksi Ervasti <n0eral00@students.oamk.fi>
2022-03-03 10:35:04 +0200

    changed button text to start new game. previous iteration was confusing by taking attention away from status.

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -178,7 +178,7 @@ export default function Gameboard() {
         <Text style={styles.buttonText}>
           {!gameOver()
             ? 'Throw dices'
-            : `Throw dices (New game)`
+            : 'New Game'
           }
         </Text>
       </Pressable>
